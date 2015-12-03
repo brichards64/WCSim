2e7cd3e048bfa2e5bb840d22c3f2edf94d6298fd
brichards64 <b.richards@qmul.ac.uk.com>
2015-12-03 01:28:13 +0000

    automated dependancies build

--- build.sh
+++ build.sh
@@ -4,14 +4,22 @@
 #rm -rf build/
 #mkdir build
 cd build
+if [ $1 = "dependancies" ]
+then
 git clone https://github.com/hyperk/hk-hyperk.git
 cd hk-hyperk
 echo '2'|./get_release.sh
 source Source_At_Start.sh
 ./build.sh build
-#cd ../WCSim
+fi
+
+if [ $1 != "dependancies" ]
+then
+cd ./WCSim
 #git checkout develop
-#source ../hk-hyperk/Source_At_Start.sh
-#make clean
-#make
+source ../hk-hyperk/Source_At_Start.sh
+make clean
+make
+fi
+
 cd ../../
