904af7e9c4ed64fd5a04ff2c4ea6a495b53f180d
brichards64 <b.richards@qmul.ac.uk.com>
2017-01-26 19:07:34 +0000

    test

--- build.sh
+++ build.sh
@@ -35,7 +35,7 @@ then
     cd ../..
     git clone http://root.cern.ch/git/root.git root
     cd root
-    ./configure  --enable-python --enable-roofit --enable-minuit2 > ../../root-build.log 2>&1
+    ./configure --disable-cxx11  --enable-python --enable-roofit --enable-minuit2 > ../../root-build.log 2>&1
     make >> ../../root-build.log 2>&1
     source bin/thisroot.sh
     echo location 3
